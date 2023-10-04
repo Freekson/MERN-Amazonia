b5da3e8a33fbd94cecebafd8ac58eb6485aa9da2
Yehor Dreval <freeksons@gmail.com>
2023-10-04 21:25:52 +0200

    conect to mongodb, create product & user model

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,24 @@
 import express from "express";
 import data from "./data.js";
-import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
+//fetch variables from .env file
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => console.log(err.message));
 
 const app = express();
-app.use(cors());
 
-// 1 par - path, 2 par - function called when user on it path
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-//path for product
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: " Product not found" });
-  }
-});
-
-//path for product
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: " Product not found" });
-  }
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
 //define port
 const port = process.env.PORT || 5000;
